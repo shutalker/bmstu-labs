7d883453e964f9017d236dd16bd5b25af9069797
Alexey Shatunov <shatunov.alexey@mail.ru>
2017-05-08 23:33:15 +0300

    Реализована адаптивная пороговая процедура определения R-зубцов

--- qrs-detecting/pan_tompkins.py
+++ qrs-detecting/pan_tompkins.py
@@ -1,5 +1,6 @@
 import wfdb
 from scipy import signal
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -58,42 +59,191 @@ def amplitude_squaring(sig):
     for sample_idx in range(len(sig)):
         sig[sample_idx] = sig[sample_idx] ** 2
 
+def peak_detection(amplitude, spk, npk, threshold, threshold_type):
+
+    peak_detector = 0
+    recalculated_thresholds = {}
+
+    if amplitude >= threshold:
+        peak_detector += 1
+        if threshold_type == 'low_threshold':
+            spk = 0.25 * amplitude + 0.75 * spk
+        else:
+            spk = 0.125 * amplitude + 0.875 * spk
+    else:
+        npk = 0.125 * amplitude + 0.875 * npk
+
+    h_threshold = npk + 0.25 * (spk - npk)
+    l_threshold = 0.5 * h_threshold
+
+    recalculated_thresholds['h_threshold'] = h_threshold
+    recalculated_thresholds['l_threshold'] = l_threshold
+    recalculated_thresholds['spk'] = spk
+    recalculated_thresholds['npk'] = npk
+    recalculated_thresholds['peak_detector'] = peak_detector
+
+    return recalculated_thresholds
+
+def adaptive_tresholds_algorithm(clean, integrated, 
+                                 search_width, peakidxs):
+
+    # initial estimates for filtered (clean) signal (in mV)
+    # Signal PeaK for Filtering waveform 
+    spkf = max(clean)
+
+    # Noise PeaK for Filtering waveform
+    npkf = spkf / 2
+
+    # adaptive thresholds (higher, lower)
+    h_threshold_f = npkf + 0.25 * (spkf - npkf)
+    l_threshold_f = 0.5 * h_threshold_f
+
+    # for Integration waveform
+    spki = max(integrated)
+    npki = spki / 3
+
+    h_threshold_i = npki + 0.25 * (spki - npki)
+    l_threshold_i = 0.5 * h_threshold_i
+
+    signal_duration = len(clean)
+    frame_amount = int(np.ceil(signal_duration / search_width))
+    start_frame_idx = 0
+    end_frame_idx = search_width
+    frame_delta = search_width * frame_amount - signal_duration
+
+    # returning value
+    pulse_signal = [0] * signal_duration
+
+    for frame_idx in range(frame_amount):
+        peak_counter = 0
+
+        if frame == frame_amount - 1:
+            end_frame_idx = search_width - frame_delta
+
+        clean_signal_frame = clean[start_frame_idx : end_frame_idx]
+        integrated_signal_frame = integrated[start_frame_idx : end_frame_idx]
+
+        # forward peak searching
+        for sample in range(start_frame_idx, end_frame_idx):
+            peak_detector = 0
+            f_amplitude = clean[sample]
+            i_amplitude = integrated[sample]
+
+            adapted_thresholds = peak_detection(f_amplitude, spkf, npkf,
+                                                h_threshold_f, 'high_threshold')
+            h_threshold_f = adapted_thresholds['h_threshold']
+            l_threshold_f = adapted_thresholds['l_threshold']
+            spkf = adapted_thresholds['spk']
+            npkf = adapted_thresholds['npk']
+            peak_detector += adapted_thresholds['peak_detector']
+
+            adapted_thresholds = peak_detection(i_amplitude, spki, npki,
+                                                h_threshold_i, 'high_threshold')
+            h_threshold_i = adapted_thresholds['h_threshold']
+            l_threshold_i = adapted_thresholds['l_threshold']
+            spki = adapted_thresholds['spk']
+            npki = adapted_thresholds['npk']
+            peak_detector += adapted_thresholds['peak_detector']
+
+            if peak_detector > 1:
+                if sample in peakidxs:
+                    pulse_signal[sample] = 1
+                    peak_counter += 1
+
+        # peek backsearch
+        if peak_counter == 0:
+            for sample in range(end_frame_idx, start_frame_idx):
+                peak_detector = 0
+                f_amplitude = clean[sample]
+                i_amplitude = integrated[sample]
+
+                adapted_thresholds = peak_detection(f_amplitude, spkf, npkf,
+                                                    l_threshold_f, 'low_threshold')
+                h_threshold_f = adapted_thresholds['h_threshold']
+                l_threshold_f = adapted_thresholds['l_threshold']
+                spkf = adapted_thresholds['spk']
+                npkf = adapted_thresholds['npk']
+                peak_detector += adapted_thresholds['peak_detector']
+
+                adapted_thresholds = peak_detection(i_amplitude, spki, npki,
+                                                    l_threshold_i, 'low_threshold')
+                h_threshold_i = adapted_thresholds['h_threshold']
+                l_threshold_i = adapted_thresholds['l_threshold']
+                spki = adapted_thresholds['spk']
+                npki = adapted_thresholds['npk']
+                peak_detector += adapted_thresholds['peak_detector']
+
+                if peak_detector > 1:
+                    if sample in peakidxs:
+                        pulse_signal[sample] = 1
+                        peak_counter += 1
+
+        start_frame_idx = end_frame_idx
+        end_frame_idx += search_width
+
+    return pulse_signal
 
 if __name__ == '__main__':
     samples_amount = 1500
-    sample_freq = 360
     lowcut = 5
     highcut = 15
     window_width = 41
 
-    rec = wfdb.rdsamp('231', sampto=samples_amount, channels=[0], physical=False, pbdir='mitdb')
-    filtering_signal = get_signal_from_channel(rec.d_signals, 0)
-    plt.subplot(311)
+    rec = wfdb.rdsamp('231', sampfrom=samples_amount*2, sampto=samples_amount*3, channels=[0],
+                      physical=True, pbdir='mitdb')
+    sample_freq = rec.fs
+    filtering_signal = get_signal_from_channel(rec.p_signals, 0)
+    plt.subplot(411)
     plt.plot(filtering_signal, 'r')
     plt.ylabel('adus')
 
     # Butterworth 1-order lowpass filter
     b, a = butter_bandpass(lowcut, sample_freq, 'lowpass')
+
     # initial condition for lfilter
     zi = signal.lfilter_zi(b, a)
-    filtering_signal, _ = signal.lfilter(b, a, filtering_signal, zi=zi*filtering_signal[0])
+    filtering_signal, _ = signal.lfilter(b, a, filtering_signal,
+                                         zi=zi*filtering_signal[0])
 
     # Butterworth 1-order highpass filter
     b, a = butter_bandpass(highcut, sample_freq, 'highpass')
     zi = signal.lfilter_zi(b, a)
-    filtering_signal, _ = signal.lfilter(b, a, filtering_signal, zi=zi*filtering_signal[0])
+    clean_signal, _ = signal.lfilter(b, a, filtering_signal,
+                                     zi=zi*filtering_signal[0])
 
-    derivative_vec = five_point_derivative(filtering_signal, sample_freq)
-    plt.subplot(312)
-    plt.plot(derivative_vec, 'g')
+    derivative_signal = five_point_derivative(clean_signal, sample_freq)
+    plt.subplot(412)
+    plt.plot(derivative_signal, 'g')
     plt.ylabel('derivative')
 
-    amplitude_squaring(filtering_signal)
-    filtering_signal = signal.medfilt(filtering_signal, window_width)
+    amplitude_squaring(clean_signal)
+    integrated_signal = signal.medfilt(clean_signal, window_width)
+
+    # finding peaks for desicion rule algorithm in filtered signal
+    peakidxs = signal.find_peaks_cwt(np.array(clean_signal), np.arange(1, 200))
+    # peaks = [0] * samples_amount
+    # for idx in range(samples_amount):
+    #     for peakidx in peakidxs:
+    #         if idx == peakidx:
+    #             peaks[idx] = 1
+
+    # plt.subplot(413)
+    # plt.plot(peaks, 'b')
+    # plt.ylabel('pulse')
+
+    # signal indicates locations of R-peaks
+    pulse_signal = adaptive_tresholds_algorithm(clean_signal,
+                                                integrated_signal,
+                                                window_width,
+                                                peakidxs)
+    #print(pulse_signal)
+    plt.subplot(413)
+    plt.plot(pulse_signal, 'b')
+    plt.ylabel('pulse')
 
     # plot resulting signal
-    plt.subplot(313)
-    plt.plot(filtering_signal, 'b')
+    plt.subplot(414)
+    plt.plot(integrated_signal)
     plt.ylabel('adus')
     plt.xlabel('filtered signal')
     plt.savefig('sig.png', format='png')
